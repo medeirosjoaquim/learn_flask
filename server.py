c923f89480dc063ac1afc78a2f0f052a38099675
Joaquim Medeiros <joaquim.medeiros@wilcompute.com>
2020-10-25 22:23:45 -0300

    connect to postgresql

--- server.py
+++ server.py
@@ -1,6 +1,40 @@
 from flask import Flask
+import psycopg2
+import psycopg2.extras
+from psycopg2 import sql
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    description = Column(String(100))
+    completed = Column(Boolean)
+
+    def __init__(self, title, description, completed):
+        self.title = title
+        self.description = description
+        self.completed = completed
+    def __repr__(self):
+            return '<Tasks {}>'.format(self.title)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    print (Tasks.query.all())
+    return "hello"
--- src/app.py
+++ src/app.py
@@ -2,19 +2,12 @@ from flask import Flask
 import psycopg2
 import psycopg2.extras
 from psycopg2 import sql
-from DBUtils.PersistentDB import PersistentDB
-
+from flas_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://api_server:docker@localhost/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-
-DATABASE = PersistentDB(
-    psycopg2,
-    host=Settings.get_setting('DB_HOST'),
-    user=Settings.get_setting('DB_USERNAME'),
-    password=Settings.get_setting('DB_PASSWORD'),
-    dbname=Settings.get_setting('DB'),
-    cursor_factory=psycopg2.extras.RealDictCursor)    
